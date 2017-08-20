8d9b774d162333ee4ed02c3e4fb2c6988d464fe1
phasenraum2010 <thomas@woehlke.org>
2017-08-20 15:00:34 +0200

    fixed #14, fixed #15

--- src/main/java/org/woehlke/simulation/evolution/desktop/SimulatedEvolutionApplication.java
+++ src/main/java/org/woehlke/simulation/evolution/desktop/SimulatedEvolutionApplication.java
@@ -6,10 +6,22 @@ import org.woehlke.simulation.evolution.view.SimulatedEvolutionFrame;
  * Class with main Method for Starting the Desktop Application.
  *
  * @see org.woehlke.simulation.evolution.view.SimulatedEvolutionFrame
+ * @see org.woehlke.simulation.evolution.view.SimulatedEvolutionApplet
+ *
+ * Simulated Evolution. Artificial Life Simulation of Bacteria Motion depending on DNA.
+ *
+ * Green food appears in a world with red moving cells.
+ * These cells eat the food if it is on their position.
+ * Movement of the cells depends on random and their DNA.
+ * A fit cell moves around and eats enough to reproduce.
+ * Reproduction is done by splitting the cell and randomly changing the DNA of the two new Cells.
+ * If a cell doesn't eat enough, it will first stand still and after a while it dies.
  *
  * &copy; 2006 - 2008 Thomas Woehlke.
- * http://thomas-woehlke.de/p/simulated-evolution/
+ *
  * @author Thomas Woehlke
+ *
+ * http://thomas-woehlke.de/p/simulated-evolution/
  */
 public class SimulatedEvolutionApplication {
 
--- src/main/java/org/woehlke/simulation/evolution/desktop/package-info.java
+++ src/main/java/org/woehlke/simulation/evolution/desktop/package-info.java
@@ -1,4 +1,11 @@
 /**
  * Starting the Desktop App.
+ *
+ * Simulated Evolution.
+ * Artificial Life Simulation of Bacteria Motion depending on DNA.
+ *
+ * &copy; 2013 Thomas Woehlke.
+ * http://thomas-woehlke.de/p/simulated-evolution/
+ * @author Thomas Woehlke
  */
 package org.woehlke.simulation.evolution.desktop;
