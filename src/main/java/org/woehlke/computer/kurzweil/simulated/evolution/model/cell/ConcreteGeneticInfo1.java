a75f200886d4c5d31ec2a5e42d8aa7d8252fc6da
JS970 <son73097@pusan.ac.kr>
2023-05-16 22:26:29 +0900

    ConcreteGeneticInfo1.java : Template Method Pattern
    ConcreteGeneticInfo1.java created for Applying Template Method Pattern
    See LifeCycle.java Commit for detail description of Template Method Pattern apply

---
+++ src/main/java/org/woehlke/computer/kurzweil/simulated/evolution/model/cell/ConcreteGeneticInfo1.java
@@ -0,0 +1,19 @@
+package org.woehlke.computer.kurzweil.simulated.evolution.model.cell;
+
+public class ConcreteGeneticInfo1 extends GeneticInfo{
+    public ConcreteGeneticInfo1 () {
+        MAX_FAT = 2000;
+        MAX_HUNGER = 1000;
+        FULL_AGE = 200;
+        FAT_MINIMUM_FOR_SEX = 800;
+        FAT_AT_BIRTH = 500;
+        FAT_PER_FOOD = 25;
+        OLD_AGE = 800;
+        MAX_AGE = 1000;
+        AGE_INC = 1;
+        FAT_DEC = 1;
+        HUNGER_INC = 1;
+        FAT_DIVISION = 2;
+    }
+
+}
