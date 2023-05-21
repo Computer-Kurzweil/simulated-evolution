58a7607629e29e6e44406d49b747b9a0ed40994f
JS970 <son73097@pusan.ac.kr>
2023-05-22 07:38:23 +0900

    CellCore.java : Factory Method Pattern
    Factory Method Pattern applied for project's extendability and
    flexibility.
    
    Abstract class of Factory Method Pattern delegates object creation to subclasses.
    
    CellCore class was originally concrete class that called from Cell
    class.
    
    After this commit, CellCore class is abstract class, and classes such as CellCoreOriginal creates concrete CellCore objects.
    
    As applying Factory Method Pattern like this, project now have
    extendability for new CellCore object which works different to
    CellCoreOriginal

--- src/main/java/org/woehlke/computer/kurzweil/simulated/evolution/model/cell/CellCore.java
+++ src/main/java/org/woehlke/computer/kurzweil/simulated/evolution/model/cell/CellCore.java
@@ -1,2 +1,23 @@
-package org.woehlke.computer.kurzweil.simulated.evolution.model.cell;public class CellCore {
+package org.woehlke.computer.kurzweil.simulated.evolution.model.cell;
+
+import java.util.List;
+import java.util.Random;
+
+abstract public class CellCore {
+    public static CellCoreOriginal createCellCore(Random random) {
+        return new CellCoreOriginal(random);
+    }
+
+    protected Random random;
+
+    protected List<Integer> dna;
+
+    protected int MIN_VALUE = 0;
+    protected int MAX_VALUE = 16;
+    protected int MAX_INITIAL_VALUE = 8;
+
+    public abstract CellCore performMitosis();
+
+    public abstract Orientation getRandomOrientation();
+
 }
